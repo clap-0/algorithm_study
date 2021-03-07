4eedfc5f98a3acc7d2d349df7935ad2ed809c72f
Sooyeong-Park <50361496+Sooyeong-Park@users.noreply.github.com>
2021-03-07 17:54:18 +0900

    Update 1068_트리.cpp

--- Sources/1068_트리.cpp
+++ Sources/1068_트리.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int del;
 vector<int> adj[50];
 bool visited[50];
-int dfs(int here){
+int dfs(int here){ //깊이우선탐색
   visited[here] = true;
   int ret=0;
   for(auto there : adj[here])
     if(!visited[there])
       ret += dfs(there);
-  if(ret==0) ret=1;
+  if(ret==0) ret=1; //리프노드인 경우
   return ret;
 }
 int main()
@@ -27,7 +27,7 @@ int main()
     else root = i;
   }
   cin >> del;
-  visited[del] = true;
+  visited[del] = true; //지운 노드는 방문 안 해도 됨
   if(!visited[root]) leafs = dfs(root);
   cout << leafs << endl;
   return 0;
