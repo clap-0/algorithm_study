d4a0e1ac77df581605e2e2abf5a648b36fb0b7fb
Sooyeong-Park <50361496+Sooyeong-Park@users.noreply.github.com>
2021-05-16 20:09:33 +0900

    Create 11280_2-SAT-3.cpp

---
+++ Sources/BOJ/11280_2-SAT-3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+const int MAX_N=10000;
+
+vector<int> adj[MAX_N*2];
+int discovered[MAX_N*2], sccId[MAX_N*2];
+bool finished[MAX_N*2];
+int counter, sccCnt;
+stack<int> stk;
+
+//참인 변수를 거짓으로, 거짓인 변수를 참으로 변환
+int Not(int x) {return x%2 ? x-1 : x+1;}
+
+//타잔의 SCC 분리 알고리즘
+int tarjanSCC(int here){
+  int ret = discovered[here] = ++counter;
+  stk.push(here);
+  for(int there : adj[here]){
+    if(!discovered[there]) ret=min(ret, tarjanSCC(there));
+    else if(!finished[there]) ret=min(ret, discovered[there]);
+  }
+  if(ret==discovered[here]){
+    while(1){
+      int tmp=stk.top(); stk.pop();
+      finished[tmp]=true;
+      sccId[tmp]=sccCnt;
+      if(tmp==here) break;
+    }
+    sccCnt++;
+  }
+  return ret;
+}
+
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+  for(int cnt=0; cnt<m; cnt++){
+    int i, j;
+    cin >> i >> j;
+    i = i>0 ? 2*(i-1) : 2*(-i-1)+1;
+    j = j>0 ? 2*(j-1) : 2*(-j-1)+1;
+    adj[Not(i)].push_back(j);
+    adj[Not(j)].push_back(i);
+  }
+  
+  for(int i=0; i<n*2; i++)
+    if(!discovered[i]) tarjanSCC(i);
+  
+  //x와 not_x가 같은 SCC에 있을 경우
+  //x가 참일 때 not_x도 참이여야 하므로 모순
+  bool sat=true;
+  for(int i=0; i<n*2; i+=2){
+    int j=i+1;
+    if(sccId[i]==sccId[j]) sat=false;
+  }
+  cout << (sat ? 1 : 0) << '\n';
+}
