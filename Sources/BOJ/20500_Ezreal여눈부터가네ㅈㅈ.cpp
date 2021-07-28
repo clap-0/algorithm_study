c1a84d4c18de8b99c40b9cfc3b245cdc794b238c
clap-0 <50361496+clap-0@users.noreply.github.com>
2021-07-28 22:03:00 +0900

    Update 20500_Ezreal여눈부터가네ㅈㅈ.cpp

--- Sources/BOJ/20500_Ezreal여눈부터가네ㅈㅈ.cpp
+++ Sources/BOJ/20500_Ezreal여눈부터가네ㅈㅈ.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 int main()
 {
-  //dp[i][j] = i자리 수 중 15로 나눈 나머지가 j인 수의 개수
-  int N, dp[1516][15]={0};
+  // 1과 5로만 구성된 15의 배수를 구해야 하므로
+  // 구하는 수는 일의 자리가 5인 3의 배수가 된다.(일의 자리가 5면 5의 배수이므로)
+  //dp[i][j] = i자리 수 중 3으로 나눈 나머지가 j인 수의 개수
+  int N, dp[1516][3]={0};
   scanf("%d", &N);
-  //반복적 동적계획법
-  dp[1][1] = dp[1][5] = 1;
-  for(int i=2; i<=N; i++)
-    for(int remainder=0; remainder<15; remainder++)
-      //모듈러 연산의 속성을 이용한다
-      if(dp[i-1][remainder]){
-        dp[i][(10+remainder)%15] = (dp[i][(10+remainder)%15] + dp[i-1][remainder])%1000000007;
-        dp[i][(50+remainder)%15] = (dp[i][(50+remainder)%15] + dp[i-1][remainder])%1000000007;
-      }
+  //15, 55
+  dp[2][0] = dp[2][1] = 1;
+  //모듈러 덧셈의 속성과
+  //(1*10^k)%3 == 1, (5*10^k)%3 == 2 (k는 자연수) 임을 이용한다
+  for(int i=3; i<=N; i++)
+    for(int r=0; r<3; r++)
+      dp[i][r] = (dp[i-1][(r+1)%3] + dp[i-1][(r+2)%3]) % 1000000007;
   printf("%d\n", dp[N][0]);
 }
