8c55f00dfbfcd06956d1bfc6207852fea0700f19
clap-0 <50361496+clap-0@users.noreply.github.com>
2021-08-20 23:07:35 +0900

    Update 10653_마라톤2.cpp

--- Sources/BOJ/10653_마라톤2.cpp
+++ Sources/BOJ/10653_마라톤2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int N, K;
-int dp[500][501], dist[501][501];
+int dp[501][501];
 pair<int,int> checkpoint[501];
 int main()
 {
@@ -9,17 +9,16 @@ int main()
   cin.tie(0); cout.tie(0);
   cin >> N >> K;
   for(int i=1; i<=N; i++) cin >> checkpoint[i].first >> checkpoint[i].second;
-  //두 체크포인트 사이의 택시거리를 미리 계산한다
-  for(int i=1; i<=N; i++)
-    for(int j=i; j<=N; j++)
-      dist[i][j]=abs(checkpoint[i].first-checkpoint[j].first)+abs(checkpoint[i].second-checkpoint[j].second);
   //반복적 동적계획법
-  for(int n=2; n<=N; n++) dp[K][n]=dp[K][n-1]+dist[n-1][n];
-  for(int k=K-1; k>=0; k--)
-    for(int n=2; n<=N; n++){
-      dp[k][n]+=dp[k][n-1]+dist[n-1][n];
-      for(int i=2; i<n&&i<K-k+2; i++)
-        dp[k][n]=min(dp[k][n], dp[k+i-1][n-i]+dist[n-i][n]);
+  for(int to=2; to<=N; to++){
+    //to번 체크포인트까지 최대 K번 건너뛸 때 각각의 횟수에 대해 큰 수로 초기화한다
+    for(int chances=0; chances<=K; chances++) dp[to][chances]=987654321;
+    for(int from=1; from<to; from++){
+      //jump: from에서 to로 건너뛸 때 건너뛰는 체크포인트의 수
+      int jump=to-from-1;
+      for(int chances=K; chances>=jump; chances--)
+        dp[to][chances-jump]=min(dp[to][chances-jump], dp[from][chances]+abs(checkpoint[from].first-checkpoint[to].first)+abs(checkpoint[from].second-checkpoint[to].second));
     }
-  cout << dp[0][N] << '\n';
+  }
+  cout << dp[N][0] << '\n';
 }
