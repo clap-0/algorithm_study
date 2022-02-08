0d45e6191345bd604e6dc56ce8a18bfa73bf3413
clap-0 <50361496+clap-0@users.noreply.github.com>
2022-02-08 21:00:49 +0900

    Update 2042_구간합구하기.cpp

--- Sources/BOJ/2042_구간합구하기.cpp
+++ Sources/BOJ/2042_구간합구하기.cpp
@@ -1,50 +1,56 @@
+// 펜윅트리를 이용해 구간 합 구하기
 #include <iostream>
 #include <vector>
-#include <cmath>
 using namespace std;
-vector<long long> arr;
-vector<long long> segTree; //세그먼트 트리
-//node 노드가 arr[left...right] 배열을 표현할때
-//node를 루트로 하는 서브트리를 초기화하고, 이 구간의 합 반환
-long long init(int node, int left, int right){
-  if(left == right) return segTree[node] = arr[left];
-  int mid = (left + right)/2;
-  return segTree[node] = init(node*2, left, mid)+init(node*2+1, mid+1, right);
+
+int N, M, K;
+vector<long long> tree;
+
+// pos위치의 값에 value만큼 더하며
+// 해당 위치를 포함하는 모든 구간의 구간 합들도 value만큼 더한다
+void add(int pos, long long value) {
+  while(pos <= N) {
+    tree[pos] += value;
+    pos += pos & -pos;
+  }
 }
-//arr[index] = newVal로 바뀌었을 때 node를 루트로하는
-//트리를 갱신하고 노드가 표현하는 구간의 합 반환
-long long update(int index, long long newVal, int node, int nodeLeft, int nodeRight){
-  if(index<nodeLeft || nodeRight<index) return segTree[node];
-  if(nodeLeft==nodeRight) return segTree[node] = newVal;
-  int mid = (nodeLeft+nodeRight)/2;
-  segTree[node] = update(index, newVal, node*2, nodeLeft, mid) + update(index, newVal, node*2+1, mid+1, nodeRight);
-  return segTree[node];
+
+// pos까지의 부분합을 구한다
+long long partialSum(int pos) {
+  long long ret = 0LL;
+  while(pos > 0) {
+    ret += tree[pos];
+    pos &= (pos - 1);
+  }
+  return ret;
 }
-//node가 표현하는 범위 arr[nodeLeft...nodeRight]가 주어질때
-//이 범위와 arr[L...R]의 교집합인 구간의 합 반환
-long long query(int L, int R, int node, int nodeLeft, int nodeRight){
-  if(R<nodeLeft || L>nodeRight) return 0;
-  if(L<=nodeLeft && nodeRight<=R) return segTree[node];
-  int mid = (nodeLeft+nodeRight)/2;
-  return query(L, R, node*2, nodeLeft, mid) + query(L, R, node*2+1, mid+1, nodeRight);
-}
+
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(0); cout.tie(0);
-  int n, m, k;
-  cin >> n >> m >> k;
-  for(int i=0; i<n; i++){
-    int num;
+
+  cin >> N >> M >> K;
+  tree = vector<long long>(N + 1, 0);
+
+  long long num;
+  for(int i = 1; i <= N; i++) {
     cin >> num;
-    arr.push_back(num);
+    add(i, num);
   }
-  segTree.resize(n*4);
-  init(1, 0, n-1);
-  for(int i=0; i<m+k; i++){
-    long long a, b, c;
+
+  for(int i = 0, cnt = M + K; i < cnt; i++) {
+    int a, b;
+    long long c;
     cin >> a >> b >> c;
-    if(a==1) update(b-1, c, 1, 0, n-1); 
-    else if(a==2) cout << query(b-1, c-1, 1, 0, n-1) << '\n';
+    if(a == 1) {
+      // 기존의 값에서 c로 값을 바꿀 때의 변화량을 구해서 해당 값만큼 더한다
+      long long variance = c - (partialSum(b) - partialSum(b - 1));
+      add(b, variance);
+    }
+    else {
+      // 부분합을 이용하여 구간 [b, c]의 구간합을 구한다
+      cout << partialSum(c) - partialSum(b - 1) << '\n';
+    }
   }
 }
