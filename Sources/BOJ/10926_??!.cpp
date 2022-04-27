6b980bc73942e73d674b174372bfa3116e03c9d6
clap-0 <50361496+clap-0@users.noreply.github.com>
2022-04-27 23:36:00 +0900

    Create 10926_??!.cpp

---
+++ Sources/BOJ/10926_??!.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+
+  string id;
+  cin >> id;
+  cout << id + "??!\n";
+}
