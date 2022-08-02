848c80ff4068425a82049d92afd91276cbfd3cde
clap-0 <50361496+clap-0@users.noreply.github.com>
2022-08-02 23:58:24 +0900

    Create 1076_저항.cpp

---
+++ Sources/BOJ/1076_저항.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+string colors[10] = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+
+  int value[3];
+  string color;
+  for(int i = 0; i < 3; i++)
+  {
+    cin >> color;
+    for(int j = 0; j < 10; j++)
+      if(colors[j] == color)
+      {
+        value[i] = j;
+        break;
+      }
+  }
+
+  cout << (value[0] * 10LL + value[1]) * pow(10LL, value[2]);
+}
