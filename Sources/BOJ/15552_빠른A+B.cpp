2ee743d86398a6e99e4f367c5bbc9503086f87ca
clap-0 <50361496+clap-0@users.noreply.github.com>
2022-08-14 23:20:13 +0900

    Create 15552_빠른A+B.cpp

---
+++ Sources/BOJ/15552_빠른A+B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
