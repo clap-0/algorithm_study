10ae887a02d71fcfda4aadf968c509cf7a935b7f
clap-0 <50361496+clap-0@users.noreply.github.com>
2022-09-11 23:16:44 +0900

    Create 2440_별찍기-3.cpp

---
+++ Sources/BOJ/2440_별찍기-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+
+  int N;
+  cin >> N;
+  for (int i = N; i > 0; i--) {
+    for (int j = 0; j < i; j++)
+      cout << "*";
+    cout << '\n';
+  }
+}
