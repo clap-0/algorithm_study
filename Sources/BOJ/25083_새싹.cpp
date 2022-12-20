2402d9dcec1aeace1fb836ff04a5fec5ade2f9f2
clap-0 <50361496+clap-0@users.noreply.github.com>
2022-12-20 23:59:54 +0900

    Create 25083_새싹.cpp

---
+++ Sources/BOJ/25083_새싹.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+
+  cout << "         ,r'\"7\n";
+  cout << "r`-_   ,'  ,/\n";
+  cout << " \\. \". L_r'\n";
+  cout << "   `~\\/\n";
+  cout << "      |\n";
+  cout << "      |\n";
+}
